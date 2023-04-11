796d71b4da573b20546f0fd4dc079bd73b4a372c
Diego Weinmann <diegoweinmann@gmail.com>
2023-04-11 19:06:19 -0300

    test

--- deploy.mjs
+++ deploy.mjs
@@ -1,21 +1,7 @@
 import "zx/globals";
 
-function rmNewLine(str) {
-  return str.replace("\n", "");
-}
+const { ARTIFACT_PATH, REF_NAME, REF } = process.env;
 
-const BRANCH = rmNewLine((await $`git branch --show-current`.quiet()).stdout);
-const SOURCE = path.join(process.cwd(), "storybook-static");
-
-const branchFolder = path.join(process.cwd(), "public", BRANCH);
-
-if (fs.exists(branchFolder)) {
-  console.log(
-    chalk.cyan(`${branchFolder} already exists. Overwritting files...`)
-  );
-  await fs.copy(SOURCE, branchFolder);
-}
-
-const metadata = {
-  branch: BRANCH,
-};
+console.log(ARTIFACT_PATH);
+console.log(REF_NAME);
+console.log(REF);
