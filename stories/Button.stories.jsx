cdd98eaa982601bf1aea419cf7a1378f4d71a26a
Diego Weinmann <diegoweinmann@gmail.com>
2023-04-10 21:44:37 -0300

    add Button stories

---
+++ stories/Button.stories.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Button() {
+  return (
+    <button
+      style={{
+        background: "purple",
+        padding: 5,
+      }}
+    >
+      My Button
+    </button>
+  );
+}
+
+const meta = {
+  title: "Button",
+  component: Button,
+};
+
+export default meta;
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/react/api/csf
+ * to learn how to use render functions.
+ */
+export const Primary = {
+  render: () => <Button />,
+};
